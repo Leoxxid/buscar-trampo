6a378cda7441b9f0ce7324474fa21305b7c30e45
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-07-29 19:57:13 -0300

    create method to find job description in details page in programathor_scraping service

--- app/services/programathor_scraping.rb
+++ app/services/programathor_scraping.rb
@@ -10,13 +10,22 @@ class ProgramathorScraping
   def jobs
     list = []
     @doc.css(".cell-list").each do |job|
-      job_url = job.xpath("a").first["href"]
+      job_url = "https://programathor.com.br/" + job.xpath("a").first["href"]
       title = job.css(".cell-list-content h3").text
       image_url = job.css(".cell-logo img").first["src"]
       company = job.css(".cell-list-content-icon span")[0].text
       locale = job.css(".cell-list-content-icon span")[1].text
-      list.push(job_url: job_url, title: title, image_url: image_url, company: company, locale: locale)
+      description = description(job_url)
+      list.push(job_url: job_url, title: title, image_url: image_url, company: company, locale: locale, description: description)
     end 
     return list
   end
+
+  private
+
+    def description(job_url)
+      doc = Nokogiri::HTML(open(job_url), nil, Encoding::UTF_8.to_s)
+      return doc.css(".wrapper-content-job-show div")[4].text.strip.delete("\n")
+    end
+    
 end
