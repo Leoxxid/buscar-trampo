dfdb2ea4887098173a03c8cda2b72e2e51310852
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-07-29 18:59:11 -0300

    implements method jobs with web scraping in programathor_scraping service

--- app/services/programathor_scraping.rb
+++ app/services/programathor_scraping.rb
@@ -8,6 +8,15 @@ class ProgramathorScraping
   end
 
   def jobs
-    @doc
+    list = []
+    @doc.css(".cell-list").each do |job|
+      job_url = job.xpath("a").first["href"]
+      title = job.css(".cell-list-content h3").text
+      image_url = job.css(".cell-logo img").first["src"]
+      company = job.css(".cell-list-content-icon span")[0].text
+      locale = job.css(".cell-list-content-icon span")[1].text
+      list.push(job_url: job_url, title: title, image_url: image_url, company: company, locale: locale)
+    end 
+    return list
   end
 end
