8c025def9977533ea2364e0af67eaec282a52e85
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-07-30 20:25:08 -0300

    write db:seed to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Tag.create(name: "Ruby on Raiils")
+Tag.create(name: "Python")
+Tag.create(name: "Java")
+Tag.create(name: "Angular")
+Tag.create(name: "React")
+
+20.times do
+  Job.create(
+    image_url: FFaker::Image.url,
+    title: FFaker::JobBR.title,
+    company: FFaker::Company.name,
+    description: FFaker::HipsterIpsum.paragraphs,
+    locale: FFaker::AddressBR.city,
+    job_url: FFaker::InternetSE.http_url,
+    tag: Tag.all.sample
+  )
+end
