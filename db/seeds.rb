be06aa0511bf8e1d513eabcdc75ef1cc911208ae
Leonardo de Almeida Rocha <leoxxid@hotmail.com>
2018-07-30 22:52:53 -0300

    Update seeds.rb
    Remove jobs creation of seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,6 @@
+# Create Tags 
 Tag.create(name: "Ruby on Rails")
 Tag.create(name: "Python")
 Tag.create(name: "Java")
 Tag.create(name: "Angular")
 Tag.create(name: "React")
-
-20.times do
-  Job.create(
-    image_url: FFaker::Image.url,
-    title: FFaker::JobBR.title,
-    company: FFaker::Company.name,
-    description: FFaker::HipsterIpsum.paragraphs,
-    locale: FFaker::AddressBR.city,
-    job_url: FFaker::InternetSE.http_url,
-    tag: Tag.all.sample
-  )
-end
