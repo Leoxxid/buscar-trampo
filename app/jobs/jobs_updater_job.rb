9134a53b7dde9758a5693aff77fcac1bb5d6f12b
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-07-30 21:23:47 -0300

    implements JobsUpdater job and config this to run every 00:00

--- app/jobs/jobs_updater_job.rb
+++ app/jobs/jobs_updater_job.rb
@@ -1,9 +1,15 @@
 class JobsUpdaterJob < ApplicationJob
   require "sidekiq-scheduler"
-  
   queue_as :scraping
 
   def perform(*args)
-    puts "working"
+    Job.destroy_all
+    Tag.all.each do |tag|
+      jobs = []
+      (1..5).each do |page|
+        jobs << ProgramathorScraping.new(tag.name.downcase.gsub(" ", "-"), page).jobs
+      end
+      jobs.flatten.each { |attrs| Job.create(attrs.merge(tag: tag))}
+    end
   end
 end
