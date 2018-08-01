73201d09eb3ca739d5578da3cc9795acae3585b4
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-07-31 21:38:30 -0300

    implement method to get job description in empregos_scraping service

--- app/services/empregos_scraping.rb
+++ app/services/empregos_scraping.rb
@@ -9,13 +9,13 @@ class EmpregosScraping
     def jobs
       list = []
       @doc.css(".col-esquerda").each do |job|
-        job_url = job.css(".grid-16-16").first.xpath("a").first["href"]
+        job_url = job.css(".descricao a").first["href"]
         title = job.css(".descricao a").first.text.strip.delete("\n")
         image_url = job.css(".grid-16-16").first.xpath("img").first["src"]
         company = job.css(".descricao .nome-empresa").text.strip.delete("\n")
         locale = job.css(".descricao .cidade-estado").text.strip.delete("\n")
-        # description = description(job_url)
-        list.push(job_url: job_url, title: title, image_url: image_url, company: company, locale: locale)
+        description = description(job_url)
+        list.push(job_url: job_url, title: title, image_url: image_url, company: company, locale: locale, description: description)
       end 
       return list
     end
@@ -24,7 +24,7 @@ class EmpregosScraping
   
       def description(job_url)
         doc = parsed_template(job_url)
-        return doc.css(".wrapper-content-job-show div")[4].text.strip.delete("\n")
+        return doc.css(".conteudo-vaga td > p").text.strip.delete("\n").delete("\r")
       end
       
       def parsed_template(url)
