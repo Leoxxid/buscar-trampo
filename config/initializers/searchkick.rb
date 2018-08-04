7e9eb7f8d7425150f17b8b40c12606f10bb19490
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-08-04 00:12:05 -0300

    fixes sidekiq jobs

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,3 +1,10 @@
 if ENV['ELASTICSEARCH_HOST']
   ENV['ELASTICSEARCH_URL'] ||= "http://#{ENV['ELASTICSEARCH_HOST']}:9200"
 end
+
+Searchkick.client_options = {
+  url: 'http://elasticsearch:9200',
+  retry_on_failure: true,
+  transport_options:
+    { request: { timeout: 250 } }
+ }
