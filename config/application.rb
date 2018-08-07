ab0826619a561f26630d59edf0d1fddc776d08ff
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-08-07 00:07:59 -0300

    change project timezone to america/sp

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module BuscarTrampo
     config.load_defaults 5.2
 
     config.active_job.queue_adapter = :sidekiq
+
+    config.time_zone = 'America/Sao_Paulo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
