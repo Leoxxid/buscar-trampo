72ee7c05086e5e0cc2664a145ade89161b67e730
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-08-07 00:07:45 -0300

    add neuvoo_scraping to jobs_updater worker

--- app/jobs/jobs_updater_job.rb
+++ app/jobs/jobs_updater_job.rb
@@ -10,6 +10,7 @@ class JobsUpdaterJob < ApplicationJob
         (1..3).each do |page|
           jobs << ProgramathorScraping.new(tag.name, page).jobs
           jobs << EmpregosScraping.new(tag.name, page).jobs
+          jobs << NeuvooScraping.new(tag.name, page)
         end
         jobs.flatten.each { |attrs| Job.create(attrs.merge(tag: tag))}
       rescue
